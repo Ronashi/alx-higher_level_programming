86a170150ace01c3a0d0d9ceccbdb42787de851b
Reagan Ronald <reaganronashi@gmail.com>
2022-12-18 08:28:27 +0300

    dede

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     r = requests.get(url)
-    if r.status_code >= 400:
+    if r.status_code  >=  400:
         print("Error code: {}".format(r.status_code))
     else:
         print(r.text)
